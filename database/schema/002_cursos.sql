15596edd8445f2a4d685cab015c2eacd656a3a26
Willkmass <williamkmass@gmail.com>
2026-08-21 18:17:48 -0300

    Delete database/schema/002_cursos.sql

--- database/schema/002_cursos.sql
+++
@@ -1,8 +0,0 @@
-create table cursos (
-    id uuid primary key,
-    nome text not null,
-    modalidade text,
-    eixo_tecnologico text,
-    carga_horaria integer,
-    created_at timestamp default now()
-);
