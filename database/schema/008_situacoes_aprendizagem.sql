2d17b97a4058ee233c03b3e0c68b7fb629d2489d
Willkmass <williamkmass@gmail.com>
2026-08-21 18:19:34 -0300

    Delete database/schema/008_situacoes_aprendizagem.sql

--- database/schema/008_situacoes_aprendizagem.sql
+++
@@ -1,8 +0,0 @@
-create table situacoes_aprendizagem (
-    id uuid primary key,
-    titulo text not null,
-    contextualizacao text,
-    desafio text,
-    resultado_esperado text,
-    created_at timestamp default now()
-);
