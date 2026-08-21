76247c95c360b4c343ff69c5c60525d63c12bc12
Willkmass <williamkmass@gmail.com>
2026-08-21 18:19:13 -0300

    Delete database/schema/018_historico_peuc.sql

--- database/schema/018_historico_peuc.sql
+++
@@ -1,8 +0,0 @@
-create table historico_peuc (
-    id uuid primary key,
-    peuc_id uuid references peucs(id) on delete cascade,
-    usuario_id uuid references users(id),
-    acao text not null,
-    descricao text,
-    created_at timestamp default now()
-);
