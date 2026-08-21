43904481fc63a30fee76c0dc0aec6517479f4342
Willkmass <williamkmass@gmail.com>
2026-08-21 18:20:20 -0300

    Delete database/schema/010_planos_aula.sql

--- database/schema/010_planos_aula.sql
+++
@@ -1,9 +0,0 @@
-create table planos_aula (
-    id uuid primary key,
-    peuc_id uuid references peucs(id) on delete cascade,
-    numero_aulas integer not null,
-    capacidade_id uuid references capacidades(id),
-    conhecimento_id uuid references conhecimentos(id),
-    criterio_id uuid references criterios_avaliacao(id),
-    created_at timestamp default now()
-);
