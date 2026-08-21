39fd735fa4d779a4ad8cdbd3a35eab152bb5ba74
Willkmass <williamkmass@gmail.com>
2026-08-21 18:20:04 -0300

    Delete database/schema/016_peuc_criterios.sql

--- database/schema/016_peuc_criterios.sql
+++
@@ -1,6 +0,0 @@
-create table peuc_criterios (
-    id uuid primary key,
-    peuc_id uuid references peucs(id) on delete cascade,
-    criterio_id uuid references criterios_avaliacao(id),
-    created_at timestamp default now()
-);
