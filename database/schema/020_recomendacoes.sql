5067067c5aeba39f0bb5f2b4daf470cbedc1037e
Willkmass <williamkmass@gmail.com>
2026-08-21 18:18:53 -0300

    Delete database/schema/020_recomendacoes.sql

--- database/schema/020_recomendacoes.sql
+++
@@ -1,9 +0,0 @@
-create table recomendacoes (
-    id uuid primary key,
-    origem_tipo text not null,
-    origem_id uuid not null,
-    destino_tipo text not null,
-    destino_id uuid not null,
-    frequencia integer default 1,
-    created_at timestamp default now()
-);
