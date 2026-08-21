e29de8470cc0bad38aaa789e1163686f4fcd848c
Willkmass <williamkmass@gmail.com>
2026-08-21 18:19:44 -0300

    Delete database/schema/015_peuc_metodologias.sql

--- database/schema/015_peuc_metodologias.sql
+++
@@ -1,6 +0,0 @@
-create table peuc_metodologias (
-    id uuid primary key,
-    peuc_id uuid references peucs(id) on delete cascade,
-    metodologia_id uuid references metodologias(id),
-    created_at timestamp default now()
-);
