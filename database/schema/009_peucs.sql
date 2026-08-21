47be05659158f4fe09ae1c69327725e6db4dd63b
Willkmass <williamkmass@gmail.com>
2026-08-21 18:19:55 -0300

    Delete database/schema/009_peucs.sql

--- database/schema/009_peucs.sql
+++
@@ -1,9 +0,0 @@
-create table peucs (
-    id uuid primary key,
-    curso_id uuid references cursos(id),
-    uc_id uuid references unidades_curriculares(id),
-    autor_id uuid references users(id),
-    status text default 'rascunho',
-    versao integer default 1,
-    created_at timestamp default now()
-);
