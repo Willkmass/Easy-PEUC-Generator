ba97e13bd88e23dde9184005edfbb4aab06cfc2f
Willkmass <williamkmass@gmail.com>
2026-08-21 18:08:59 -0300

    Delete types/peuc.ts

--- types/peuc.ts
+++
@@ -1,9 +0,0 @@
-export interface PEUC {
-  id: string;
-  curso: string;
-  unidadeCurricular: string;
-  objetivo: string;
-  contextualizacao: string;
-  desafio: string;
-  resultadosEsperados: string;
-}
