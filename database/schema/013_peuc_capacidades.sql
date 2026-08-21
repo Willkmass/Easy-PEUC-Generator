28781e77619c57808bd31bbed75c27d9f9b7f562
Willkmass <williamkmass@gmail.com>
2026-08-21 18:20:48 -0300

    Delete database/schema/013_peuc_capacidades.sql

--- database/schema/013_peuc_capacidades.sql
+++
@@ -1,6 +0,0 @@
-create table peuc_capacidades (
-    id uuid primary key,
-    peuc_id uuid references peucs(id) on delete cascade,
-    capacidade_id uuid references capacidades(id),
-    created_at timestamp default now()
-);
