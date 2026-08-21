60697c68793bc9b7db2a703d455a0af5a466e131
Willkmass <williamkmass@gmail.com>
2026-08-21 18:20:29 -0300

    Delete database/schema/014_peuc_conhecimentos.sql

--- database/schema/014_peuc_conhecimentos.sql
+++
@@ -1,6 +0,0 @@
-create table peuc_conhecimentos (
-    id uuid primary key,
-    peuc_id uuid references peucs(id) on delete cascade,
-    conhecimento_id uuid references conhecimentos(id),
-    created_at timestamp default now()
-);
