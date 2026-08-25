7424b17fca0786eaa2886072595b2f0303941bbc
Willkmass <williamkmass@gmail.com>
2026-08-25 16:41:26 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Easy PEUC Generator",
@@ -13,7 +14,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className="bg-slate-50 min-h-screen text-slate-800 antialiased flex flex-col">
+        {/* Componente de Navegação Global */}
+        <Navbar />
+        
+        {/* Conteúdo da Rota Ativa */}
+        <main className="flex-1 py-6">{children}</main>
+      </body>
     </html>
   );
 }
