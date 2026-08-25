8a0f0be3adf7b8e7a8200a1273eb34ea8acdf394
Willkmass <williamkmass@gmail.com>
2026-08-25 19:16:39 -0300

    Delete components directory

--- components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t bg-white py-4">
-      <div className="mx-auto max-w-7xl px-4 text-center text-sm text-slate-500">
-        Easy PEUC Generator © 2026
-      </div>
-    </footer>
-  );
-}
