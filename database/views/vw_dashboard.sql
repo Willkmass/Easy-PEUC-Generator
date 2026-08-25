868304c019bea4c573f6de2e8e01f494a632e5cc
Willkmass <williamkmass@gmail.com>
2026-08-25 19:16:24 -0300

    Delete database directory

--- database/views/vw_dashboard.sql
+++
@@ -1,6 +0,0 @@
-create view vw_dashboard as
-select
-    (select count(*) from cursos) as total_cursos,
-    (select count(*) from unidades_curriculares) as total_ucs,
-    (select count(*) from peucs) as total_peucs,
-    (select count(*) from users) as total_usuarios;
