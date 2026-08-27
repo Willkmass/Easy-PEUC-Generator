646f9eee52e03ce87ad577fa97deff9c3611d6fb
Willkmass <williamkmass@gmail.com>
2026-08-27 14:52:29 -0300

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Desativa os indicadores e ferramentas de desenvolvimento/plataforma
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+  },
+};
+
+export default nextConfig;
